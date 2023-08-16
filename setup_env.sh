41005632a797d8a1243cae602e4dd350c829e2b4
Zhang Yan <yan3.zhang@intel.com>
2023-08-16 22:24:57 +0800

    update env

--- setup_env.sh
+++ setup_env.sh
@@ -7,9 +7,9 @@ WORK_DIR=$PWD
 # Prerequsite: conda, python 3.8 or above; cmake; LLVM-13
 
 # Step 0: Create conda environment & install related packages
-#conda create -n ipex python=3.8 -y
-#conda activate ipex
-#conda install pytorch==1.13.0 torchvision==0.14.0 -c pytorch
+conda create -n ipex python=3.8 -y
+conda activate ipex
+conda install pytorch==1.13.0 torchvision==0.14.0 -c pytorch
 
 # Step 1: Build IPEX
 git clone --branch release/1.13 https://github.com/intel/intel-extension-for-pytorch
