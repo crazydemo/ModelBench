1673a126881c8088caecd4e6d61d25b7051b72f9
Zhang Yan <yan3.zhang@intel.com>
2023-09-05 23:57:26 +0800

    disable ipex.optimize when bench fp32

--- bench_dlrm.sh
+++ bench_dlrm.sh
@@ -39,6 +39,8 @@ cd ..
 # navigate to bert_large dir
 cd quickstart/recommendation/pytorch/dlrm/inference/cpu
 export LD_PRELOAD=${LD_PRELOAD}:/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+sed -i 's/dlrm = ipex.optimize/#dlrm = ipex.optimize/g' ${MODEL_DIR}/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py
+sed -i 's/dlrm, optimizer = ipex.optimize/#dlrm, optimizer = ipex.optimize/g' ${MODEL_DIR}/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py
 
 # bench fp32 bert large with batchsize = 32 / 512
 _ONEDNN_GRAPH_DISABLE_COMPILER_BACKEND=0 numactl -C 0-31 -m 0 python -u ${MODEL_DIR}/models/recommendation/pytorch/dlrm/product/dlrm_s_pytorch.py --raw-data-file=${DATASET_DIR}/day --processed-data-file=${DATASET_DIR}/terabyte_processed.npz --data-set=terabyte --memory-map --mlperf-bin-loader --round-targets=True --learning-rate=1.0 --arch-mlp-bot=13-512-256-128 --arch-mlp-top=1024-1024-512-256-1 --arch-sparse-feature-size=128 --max-ind-range=40000000 --ipex-interaction --numpy-rand-seed=727 --inference-only --num-batches=1000 --print-freq=10 --print-time --mini-batch-size=32 --share-weight-instance=32 | tee fp32_bs32_onednn_graph_compiler.log 2>&1
